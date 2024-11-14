a9d590203c018ea987bb1943c0642ca049a63a32
Ian Gilmore <ian.gilmore@me.com>
2024-11-14 14:22:30 -0500

    Move Navbar & page formatting to Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import Link from "next/link";
 
-import { Navbar } from "./_components/Navbar";
 import { PackageList } from "./_components/PackageList";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center  bg-gradient-to-b from-[rgb(46,2,109)] to-[#15162c] text-white">
-      <Navbar />
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <>
         <h1 className="text-2xl font-extrabold tracking-tight text-white sm:text-[4rem]">
           Packages
         </h1>
@@ -17,7 +14,6 @@ export default function HomePage() {
             Add Package
           </button>
         </Link>
-      </div>
-    </main>
+      </>
   );
 }
