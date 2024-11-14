8d302d1ba3faeae57d2573d176874e8035b77785
Ian Gilmore <ian.gilmore@me.com>
2024-11-14 13:28:41 -0500

    scaffold AppDetail & UninstallModal components

---
+++ src/app/_components/AppDetail.tsx
@@ -0,0 +1,8 @@
+export function AppDetail() {
+    return (
+        <div className="app-detail">
+            <h1>App Detail</h1>
+            <p>This is the detail of the application</p>
+        </div>
+    );
+}
---
+++ src/app/_components/UninstallModal.tsx
@@ -0,0 +1,14 @@
+interface UninstallModalProps {
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+export function UninstallModal({ onClose, onConfirm }: UninstallModalProps) {
+  return (
+      <div className="uninstall-modal">
+          <p>Are you sure you want to uninstall this application?</p>
+          <button onClick={onConfirm}>Uninstall</button>
+          <button onClick={onClose}>Cancel</button>
+      </div>
+  );
+}
